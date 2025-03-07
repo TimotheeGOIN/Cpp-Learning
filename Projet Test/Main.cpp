6e00b5157f8e35c9a3876c242c2414006e7e2c70
TimotheeGOIN <timothee.gm@gmail.com>
2025-03-08 00:35:57 +0100

    Manipulation d'array

--- Projet Test/Main.cpp
+++ Projet Test/Main.cpp
@@ -2,10 +2,27 @@
 #include <iostream>
 #include "Function.hpp"
 
+void compute_table(int table, int output[11]) {
+
+	for (int i = 0; i <= 10; ++i) {
+		output[i] = i * table;
+	}
+}
+
+void display_table(int table[11]) {
+
+	for (int i = 0; i <= 10; ++i) {
+		std::cout << i << " x " << table[1] << " = " << table[i] << std::endl;
+	}
+}
+
 int main() {
 
-	std::cout << add_custom(2, 7) << std::endl;
-	std::cout << add_custom(7.3f, 5.9f) << std::endl;
+	int table[11] = {0};
+
+	compute_table(9, table);
+	
+	display_table(table);
 
 	return 0;
 }
